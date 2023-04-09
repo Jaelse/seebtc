dee3844faa6aae6f5ecfcf895de59c9640093cc9
jaelse <j.ronald@hydroneo.net>
2023-04-09 18:40:57 +0700

    initial commit

---
+++ src/main/java/com/jaelse/seebtc/SeebtcApplication.java
@@ -0,0 +1,13 @@
+package com.jaelse.seebtc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeebtcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeebtcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaelse/seebtc/SeebtcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaelse.seebtc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeebtcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
